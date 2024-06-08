59acfea76588c81ecadb8daa1304aee188f71ab4
osmartinez <its.osmartinez@gmail.com>
2024-06-08 10:03:45 +0200

    wip post single page

--- angular-routes/src/app/pages/posts/post-detail/post-detail.component.ts
+++ angular-routes/src/app/pages/posts/post-detail/post-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Post } from '../../../interfaces/post';
+import { PostService } from '../../../services/post.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail',
@@ -8,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrl: './post-detail.component.css'
 })
 export class PostDetailComponent {
+  post: Post|null= null
+  parametro: string|null = "nada"
+
+  constructor(private postService: PostService, private route: ActivatedRoute){
+    route.paramMap.subscribe(params=> {
+      this.parametro = params.get('id')
+    })
+    postService.getById(Number(this.parametro)).subscribe({
+      next: (response)=>{
+        this.post = response as Post
+      },
+      error: ()=>{
+
+      }
+    })
+  }
 
 }
