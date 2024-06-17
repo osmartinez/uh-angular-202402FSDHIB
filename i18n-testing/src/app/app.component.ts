9b01baeb266bc05a38d756b412ccc8df8dbc2920
osmartinez <its.osmartinez@gmail.com>
2024-06-17 20:25:48 +0200

    fin i18n

---
+++ i18n-testing/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
+export function HttpLoaderFactory(httpClient: HttpClient){
+  return new TranslateHttpLoader(httpClient)
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,TranslateModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'i18n-testing';
+
+  constructor(public translate: TranslateService){
+    const misIdiomas = ['es','en','fr']
+    translate.addLangs(misIdiomas)
+    translate.setDefaultLang('en') // idioma de fallback
+
+    // const idiomaNavegador: string = translate.getBrowserLang() === undefined ? translate.getDefaultLang() : translate.getBrowserLang()
+    const idiomaNavegador: string = translate.getBrowserLang() ?? translate.getDefaultLang()
+    if(misIdiomas.includes(idiomaNavegador)){
+      translate.use(idiomaNavegador)
+    }
+    else{
+      translate.use(translate.getDefaultLang())
+    }
+
+  }
+
+  cambiarIdioma(event: Event){
+    const elementoHtml = event.target as HTMLSelectElement
+    this.translate.use(elementoHtml.value)
+
+  }
+
+
+}
---
+++ i18n-testing/src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpLoaderFactory } from './app.component';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideHttpClient(),
+    importProvidersFrom(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }))
+  ]
+};
